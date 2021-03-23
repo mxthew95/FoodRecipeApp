013a44c18649aa49793aa8e551e71b39669e09b4
mxthew95 <mxthew95@gmail.com>
2021-03-23 18:44:30 +0800

    Added bare rn project

--- App.js
+++ App.js
@@ -5,13 +5,43 @@ import {
   View,
   SafeAreaView
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', { name: 'Jane' })
+      }
+    />
+  );
+};
+const ProfileScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+/*
+<NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+*/
 
 export default function App(){
 
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>Dummy...</Text>
+        <Text>dummy...</Text>
       </View>
     </SafeAreaView>
   );
